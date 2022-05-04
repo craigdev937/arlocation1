8990e2c6905fe36dad393996d9d9ae4a203d0712
Craig <indigomx9@gmail.com>
2022-05-03 23:32:59 -0400

    Updated Fetch.

--- src/global/Fetch.ts
+++ src/global/Fetch.ts
@@ -2,15 +2,15 @@ import { PostType } from "../models/Types";
 const URL = "https://jsonplaceholder.typicode.com/posts";
 
 class FetchClass {
-    fetchPosts = async () => {
+    fetchPosts = async (): Promise<PostType[]> => {
         const res: Response = await fetch(URL);
         await new Promise((res) => setTimeout(res, 2000));
         if (!res.ok) throw new Error(res.statusText);
         const data: PostType[] = await res.json();
         return [...data.slice(0, 5)];
     };
-    
-    fetchPostById = async (postId: string) => {
+
+    fetchPostById = async (postId: string): Promise<PostType> => {
         const res: Response = await fetch(`${URL}/${postId}`);
         await new Promise((res) => setTimeout(res, 2000));
         if (!res.ok) throw new Error(res.statusText);
